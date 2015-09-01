6ed93c7bd2e4fb6b7873c216fa6d03d8c500e90e
sp500 <huaibin.yang@gmail.com>
2015-09-01 01:37:07 -0700

    print out alltime highs

--- process-sp500.py
+++ process-sp500.py
@@ -2,13 +2,34 @@
 
 import re
 
-
 sp500_data = []
-
 
 with open('sp500-history.csv') as file:
     sp500_data = file.readlines()
 
+def convert_csv_to_array( sp500_data ):
+    "convert each line to arry with closing price"
+    
+    sp500_array = []
+    
+    for line in sp500_data:
+        tmp = line.split(',')
+        tmp[6] = tmp[6].rstrip('\r\n')
+        tmp[6] = float(tmp[6])
+        sp500_array.append([tmp[0], tmp[6]])
+
+    return sp500_array
+
+def find_alltime_highs(sp500_array):
+    "find sp500 all time highs"
+    
+    max = 0;
+    sp500_alltime_highs = []
+    for i in range (1, len(sp500_array)):
+        if sp500_array[-i][1] > max:
+            max = sp500_array[-i][1]
+            sp500_alltime_highs.append(sp500_array[-i])
+    return sp500_alltime_highs
 
 def find_sp500_month_data( sp500_data ):
     "reduce data set including only the last day of each month"
@@ -24,12 +45,22 @@ def find_sp500_month_data( sp500_data ):
                 sp500_data_month.append(line)
                 month_pre = month_cur
 
-    return sp500_data_month;
+    return sp500_data_month
 
 
-sp500_m_d = find_sp500_month_data(sp500_data)
-for num in range (1, len(sp500_m_d)):
-    print sp500_m_d[-num].strip('\n')
+sp500_array = convert_csv_to_array(sp500_data)
+sp500_alltime_highs = find_alltime_highs(sp500_array)
+for tmp in sp500_alltime_highs:
+    print tmp;
+
+
+# for tmp in sp500_array:
+#     print tmp[1]
+
+# sp500_m_d = find_sp500_month_data(sp500_data)
+
+# for num in range (1, len(sp500_m_d)):
+#     print sp500_m_d[-num].strip('\n')
 
 
 
