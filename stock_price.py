d6fe9e8336aef2ca71473b247c040dca0955412b
sp500 <huaibin.yang@gmail.com>
2016-01-29 00:48:32 -0800

    stock_price: add command line for data dir

--- stock_price.py
+++ stock_price.py
@@ -1,13 +1,33 @@
+#!/usr/bin/env python
 
+import os
 import numpy as np
 import pandas as pd
-from pandas_datareader import data
-import datetime
+import pandas_datareader.data as pr
+import datetime as dt
+import argparse as ap
 
 class stock_data:
     def __init__(self, ticker):
-        self.default_start = datetime.datetime(1950, 1, 1)
+        self.default_start = dt.datetime(1950, 1, 1)
         self.ticker = ticker
-        self.price = data.DataReader(ticker, "yahoo", start=self.default_start)
+        self.price = pr.DataReader(ticker, "yahoo", start=self.default_start)
 
-stock_data("QCOM")
+def get_data_dir():
+    parser = ap.ArgumentParser(description='Get stock price')
+    parser.add_argument("data_dir")
+    cmd_args = parser.parse_args()
+    data_dir = os.path.abspath(cmd_args.data_dir)
+    if not os.path.exists(data_dir):
+        print "%s does not exist!" % src_dir
+        sys.exit()
+
+    return data_dir
+
+def main():
+    data_dir = get_data_dir()
+    print data_dir
+    stock_data("QCOM")
+
+if __name__ == "__main__":
+    main()
