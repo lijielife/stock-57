f914dbce8f887a0423c425bbd5cf2ebe9a67e1a6
sp500 <huaibin.yang@gmail.com>
2016-01-30 22:36:01 -0800

    store stock price works

--- stock_price.py
+++ stock_price.py
@@ -6,12 +6,41 @@ import pandas as pd
 import pandas_datareader.data as pr
 import datetime as dt
 import argparse as ap
+import tables as tb
+
+
+default_start = dt.datetime(1950, 1, 1)
+default_url = 'yahoo'
+default_file = 'stock_data.h5'
 
 class stock_price:
-    def __init__(self, ticker, data_dir):
-        self.default_start = dt.datetime(1950, 1, 1)
+    def __init__(self, ticker):
+        # build stock price in pytable
         self.ticker = ticker
-        self.price = pr.DataReader(ticker, "yahoo", start=self.default_start)
+
+    def store_data(self, data_file):
+        st = pd.HDFStore(data_file)
+        st_node_dir = '/price/' + self.ticker
+
+        node = st.get_storer(st_node_dir)
+        if node == None:
+            start = default_start
+        else:
+            nrows = node.table.nrows
+            if nrows > 0:
+                start = st.select(st_node_dir, where=[nrows - 1],
+                                 columns=['Date']).index[0]
+            else:
+                start = default_start
+
+        diff = dt.datetime.today() - start
+        if diff.days > 1:
+            price = pr.DataReader(self.ticker, "yahoo", start=start)
+            st.append(st_node_dir, price)
+        else:
+            print "%s is already updated!" % self.ticker
+
+        st.close()
 
 def get_cmd_line():
     parser = ap.ArgumentParser(description='update stock price')
@@ -27,6 +56,11 @@ def get_cmd_line():
 
 def main():
     cmd_line = get_cmd_line()
+    symbol = cmd_line[0]
+    folder = cmd_line[1]
+    stfile = folder + '/' + default_file
+    price = stock_price(symbol)
+    price.store_data(stfile)
 
 if __name__ == "__main__":
     main()
