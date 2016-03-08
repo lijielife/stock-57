5ae60b6b68994ef0c5a83bec36d7c08a93de8590
sp500 <huaibin.yang@gmail.com>
2016-03-07 20:28:16 -0800

    analysis: income and balance dataframe

--- stock_analysis.py
+++ stock_analysis.py
@@ -4,17 +4,15 @@ import os
 import locale
 import pandas as pd
 import matplotlib.pyplot as plt
-from stock_symbols import select_symbols
+from stock_symbols import pick_symbols
 from stock_financial import rename_columns_back
-
 
 file_price = '/home/yangh/ws/stock-data/prices.h5'
 file_financial = '/home/yangh/ws/stock-data/financials.h5'
 
 locale.setlocale(locale.LC_NUMERIC, '')
 
-sel = select_symbols('Capital Goods', 'Electronic Components')
-symbol = 'QCOM'
+sel = pick_symbols()
 
 nodes = ['/%s/ratio', '/%s/income/Annual', '/%s/income/Quarterly',
          '/%s/balance_sheet/Quarterly', '/%s/balance_sheet/Annual',
@@ -35,41 +33,93 @@ def get_financial_data(node):
     data.set_index('report', inplace=True)
     return data
     
-def plot_data_and_change(data, title):
+def plot_data_and_change(symbol, data, title):
     chg = data / data.shift(1) - 1
 
     fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12,4))
 
-    data.plot(ax=axes[0], kind='bar', color=set_color(data), title=title)
-    chg.plot(ax=axes[1], kind='bar', color=set_color(chg), title='change')
+    data.plot(ax=axes[0], kind='bar', color=set_color(data),
+              title="%s %s" %(symbol, title))
+    chg.plot(ax=axes[1], kind='bar', color=set_color(chg),
+             title='%s %s change' %(symbol, title))
 
+def plot_item(symbol, title, y_data, q_data):
+    plot_data_and_change(symbol, y_data[title], title)
+    plot_data_and_change(symbol, q_data[title], title)
 
-def get_revenue(financials):
-    return [financials[0].loc['Revenue USD Mil'].apply(locale.atof),
-            financials[2].loc['Revenue']]
+def get_income_annual(financials):
+    y_income = pd.DataFrame()
+    y_income['revenue'] = financials[0].loc['Revenue USD Mil'].apply(locale.atof);
 
-def get_gross_profit(financials):
-    return [financials[0].loc['Gross Margin %'].apply(locale.atof) *
-            financials[0].loc['Revenue USD Mil'].apply(locale.atof),
-            financials[2].loc['Gross profit']]
+    gross_margin = financials[0].loc['Gross Margin %'].apply(locale.atof) / 100.0
+    y_income['gross_profit'] = y_income['revenue'] * gross_margin
+    y_income['gross_margin'] = gross_margin
 
-def get_gross_margin(financials):
-    q_gross_margin = financials[2].loc['Gross profit'] /\
-                     financials[2].loc['Revenue']
+    y_income['op_income'] = financials[0].loc['Operating Income USD Mil'].\
+                apply(locale.atof)
+    y_income['op_margin'] = y_income['op_income'] / y_income['revenue']
 
-    return [financials[0].loc['Gross Margin %'].apply(locale.atof),
-            q_gross_margin]
+    y_income['net_income'] = financials[0].loc['Net Income USD Mil'].\
+                apply(locale.atof)
+    y_income['net_margin'] = y_income['net_income'] / y_income['revenue']
 
-def get_op_income(financials):
-    return [financials[0].loc['Operating Income USD Mil'].apply(locale.atof),
-            financials[2].loc['Operating income']]
+    y_income['cash_flow'] = financials[0].loc['Operating Cash Flow USD Mil'].\
+                apply(locale.atof)
+    y_income['cash_flow_margin'] = y_income['cash_flow'] / y_income['revenue']
 
-def get_op_margin(financials):
-    q_op_margin = financials[2].loc['Operating income'] /\
-                  financials[2].loc['Revenue']
+    y_income['free_cash_flow'] = financials[0].loc['Free Cash Flow USD Mil'].\
+                apply(locale.atof)
+    y_income['free_cash_margin'] = y_income['free_cash_flow'] / y_income['revenue']
 
-    return [financials[0].loc['Operating Margin %'].apply(locale.atof),
-            q_op_margin]
+    return y_income
+
+def get_income_quarterly(financials):
+    q_income = pd.DataFrame()
+    q_income['revenue'] = financials[2].loc['Revenue']
+
+    q_income['gross_profit'] = financials[2].loc['Gross profit']
+    q_income['gross_margin'] = q_income['gross_profit'] / q_income['revenue']
+
+    q_income['op_income'] = financials[2].loc['Operating income']
+    q_income['op_margin'] = q_income['op_income'] / q_income['revenue']
+
+    q_income['net_income'] = financials[2].loc['Net income']
+    q_income['net_margin'] = q_income['net_income'] / q_income['revenue']
+
+    q_income['cash_flow'] = financials[5].loc['Operating cash flow']
+    q_income['cash_flow_margin'] = q_income['cash_flow'] / q_income['revenue']
+
+    q_income['free_cash_flow'] = financials[5].loc['Free cash flow']
+    q_income['free_cash_margin'] = q_income['free_cash_flow'] /\
+            q_income['revenue']
+
+    return q_income
+
+def get_balance_anual(financials):
+    y_balance = pd.DataFrame()
+    y_balance['cash_value'] = financials[4].loc['Total cash']
+    y_balance['long_term_debt'] = financials[4].\
+            loc['Total non-current liabilities']
+    y_balance['net_cash_value'] = y_balance['cash_value'] - \
+            y_balance['long_term_debt']
+
+    y_balance['net_cash_per_share'] = y_balance['net_cash_value'] /\
+            financials[1].loc['Basic'].iloc[1]
+
+    return y_balance
+
+def get_balance_quarterly(financials):
+    q_balance = pd.DataFrame()
+    q_balance['cash_value'] = financials[3].loc['Total cash']
+    q_balance['long_term_debt'] = financials[3].\
+            loc['Total non-current liabilities']
+    q_balance['net_cash_value'] = q_balance['cash_value'] - \
+            q_balance['long_term_debt']
+
+    q_balance['net_cash_per_share'] = q_balance['net_cash_value'] /\
+            financials[2].loc['Basic'].iloc[1]
+
+    return q_balance
 
 def plot_financials(symbol):
     price = get_price_data(symbol)
@@ -78,12 +128,18 @@ def plot_financials(symbol):
     plt.figure()
     price['2006-12-01'::].plot(grid=True, figsize=(12,4), title=symbol)
 
-    map(lambda x: plot_data_and_change(x, 'revenue'), get_revenue(financials))
-    map(lambda x: plot_data_and_change(x, 'gross_margin'),
-            get_gross_margin(financials))
-    map(lambda x: plot_data_and_change(x, 'op_income'),
-            get_op_income(financials))
-    map(lambda x: plot_data_and_change(x, 'op_margin'),
-            get_op_margin(financials))
+    y_in = get_income_annual(financials)
+    q_in = get_income_quarterly(financials)
+
+    y_bs = get_balance_anual(financials)
+    q_bs = get_balance_quarterly(financials)
+
+    item = ['revenue', 'op_income', 'op_margin', 'net_income', 'cash_flow',
+            'free_cash_flow']
+
+    map(lambda x: plot_item(symbol, x, y_in, q_in), item)
+
+    item = ['net_cash_value', 'net_cash_per_share']
+    map(lambda x: plot_item(symbol, x, y_bs, q_bs), item)
 
 map(lambda x: plot_financials(x), sel['Symbol'])
